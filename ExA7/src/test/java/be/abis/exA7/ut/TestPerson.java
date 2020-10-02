26d756f720d8816f4750d822a1ffb58b0eb4fbde
geertlambrechts <geert.lambrechts@bnpparibasfortis.com>
2020-10-02 15:54:08 +0200

    ExA7 gives error because exception is not caught

---
+++ ExA7/src/test/java/be/abis/exA7/ut/TestPerson.java
@@ -0,0 +1,54 @@
+package be.abis.exA7.ut;
+
+import be.abis.exA7.exceptions.PersonShouldBeAdultException;
+import be.abis.exA7.model.Person;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class TestPerson {
+    Person person;
+
+    @Before
+    public void setUp() {
+        person = new Person(2, "John", "Doe", LocalDate.of(1967, 8, 10));
+        System.out.println("person created in setUp");
+    }
+
+    @Test
+    public void testCalculateAge() {
+        //arrange
+
+        Integer expected = 53;
+
+        //act
+        Integer result = person.calculateAge();
+
+        // assert
+        MatcherAssert.assertThat(result, CoreMatchers.is(CoreMatchers.equalTo(expected)));
+    }
+
+    @Test
+    public void toStringSentenceStartsWithPerson() {
+        //arrange
+        String expected = "Person";
+
+        //act
+
+        String result = person.toString();
+
+        // assert
+        MatcherAssert.assertThat(result, CoreMatchers.startsWith(expected));
+    }
+
+    @Test(expected = PersonShouldBeAdultException.class)
+    public void testPersonShouldBeAdultException () {
+        //act
+        person.calculateAge();
+    }
+
+}
+
